58247cbac74bd2835f433d1b296be9db501f1228
theshreelaxmi <theshreelaxmi@gmail.com>
2025-07-27 10:23:14 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Shree Laxmi</title>
+</head>
+
+<body>
+    <center>
+        <h1>The Heading</h1>
+    </center>
+</body>
+
+</html>
